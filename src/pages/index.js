c73e9145bd11e712767085eb8cc9a62182f3a924
lecarles <35672759+lecarles@users.noreply.github.com>
2018-05-06 22:48:50 +0200

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,30 @@
 import React from "react";
-import styles from "./index.module.css";
+import styles from "./indexhi.module.css";
 import Link from 'gatsby-link';
 
 
 export default () => (
-  <content>
   <div className={styles.wrapper}>
-	 <div className={styles.bgline}>
-	  	<svg height="810" width="810">
-	        <line className={styles.str} x1="100" y1="200" x2="850" y2="850"/>
-	    </svg>
-	  </div>
+ 
+  	<div className={styles.project}>
+  		
+      <div className={styles.bgline}>
+        <svg height="125" width="125">
+          <line className={styles.str} x1="2" y1="2" x2="120" y2="100"/>
+        </svg>
+      </div>
 
-	<div className={styles.picture}><Link to="/visoren/">.</Link></div>  
-  	<div className={styles.project}> <Link to="/visoren/">
-  		<h2>visoren fest 5</h2> 
-   	 	<h3>_Brand Identity _Digital </h3>
-    	<p>Work for the Visoren Music Festival, held in Gandia and organized by the Students' Residence and the University.</p> 
-	</Link>
+      <h2>hi!</h2> 
+
+    	<p>I’m <mark>Carles Ibáñez</mark>, 20 year old audiovisual communication student, freelance designer, and Marshall McLuhan fanboy</p>
+
+      <p>I’m currently programming this personal page/portfolio, come back later to see how it turned out!</p>
+
+      <p>meanwhile you can check my <a href="../img/carles_pf.pdf">pdf portfolio</a>, visit my behance or email me at carles2612@gmail.com</p>
   	</div>
 
-		<svg className={styles.next} height="130" width="130">
-	        
-	        <circle className={styles.str} cx="63" cy="63" r="54"/>
-	        <line className={styles.str} x1="40" y1="63" x2="80" y2="63"/>
-	        
-	        <line className={styles.str} x1="68" y1="83" x2="83" y2="61"/>
-	        <line className={styles.str} x1="68" y1="43" x2="83" y2="65"/>
-	    </svg>
 
-	    <svg className={styles.page} height="130" width="130">
-	        
-	        <circle className={styles.str} cx="63" cy="63" r="54"/>
-	        <p>1/5</p>
-
-	    </svg>
 
   </div>
-  </content>
+
 );
